8166f2e6322e241d15a41f51523cd07a7bdf8bba
HouNingZhou <allcky@qq.com>
2015-06-03 22:20:39 +0800

    修改 js

--- js/hnz_index.js
+++ js/hnz_index.js
@@ -1,125 +1,105 @@
-$$(function(){
-    //banner
-    var banl=$$(".hnz_banner_list")[0];
-    var blinks=$$("a",banl);
-    var lbtn=$$(".hnz_btn_l")[0];
-    var rbtn=$$(".hnz_btn_r")[0];
-    var bnum=0;
-    blinks[bnum].style.zIndex=1;
-    var at=setInterval(bannerMove,3000);
-    function bannerMove(){
-        bnum++;
-        if(bnum==blinks.length){
-            bnum=0;
-        }
-        for(var i=0;i<blinks.length;i++){
-            blinks[i].style.zIndex=0;
-        }
-        blinks[bnum].style.zIndex=1;
-    }
-    lbtn.onclick=function(){
-        bnum++;
-        if(bnum==blinks.length){
-            bnum=0;
-        }
-        for(var i=0;i<blinks.length;i++){
-            blinks[i].style.zIndex=0;
-        }
-        blinks[bnum].style.zIndex=1;
-    }
-    rbtn.onclick=function(){
-        bnum--;
-        if(bnum<0){
-            bnum=blinks.length-1;
-        }
-        for(var i=0;i<blinks.length;i++){
-            blinks[i].style.zIndex=0;
-        }
-        blinks[bnum].style.zIndex=1;
-    }
-    lbtn.onmouseover=function(){
-        clearInterval(at)
-    }
-    lbtn.onmouseout=function(){
-        at=setInterval(bannerMove,3000);
-    }
-    rbtn.onmouseover=function(){
-        clearInterval(at)
-    }
-    rbtn.onmouseout=function(){
-        at=setInterval(bannerMove,3000);
-    }
-    //¿ìËÙµ¼º½
-    var qnav_list=$$(".hnz_qnav_l_list")[0];
-    var qnav_box=$$(".hnz_qnav_l_box")[0];
-    var qnav_one=$$("li",qnav_box);
-    var qoneh=qnav_one[0].offsetHeight;
-    var qnav_listh=qnav_one.length*qoneh;
-    qnav_list.style.height=qnav_listh+"px";
-    var btnUp=$$(".hnz_btn_up")[0];
-    var btnDown=$$(".hnz_btn_down")[0];
-    var outboxh=qnav_box.offsetHeight;
-    var max=Math.ceil(qnav_one.length/5);
-    var num=0;
-    if(qnav_listh>outboxh){
-        btnUp.onclick=function(){
-            num++;
-            if(num>=max){
-                num=max-1;
-                return;
-            }else{
-                animate(qnav_list,{"marginTop":-5*num*qoneh})
-            }
-        }
-        btnDown.onclick=function(){
-            num--;
-            console.log(num)
-            if(num<0){
-                num=0;
-                return;
-            }else{
-                animate(qnav_list,{"marginTop":-5*num*qoneh})
-            }
-        }
-    }
-    /*¿ìËÙµ¼º½ÓÒ²àtab*/
-    var qlinks=$$("a",$$(".hnz_qnav_l_list")[0]);
-    var qlists=$$("li",$$(".hnz_qnav_r")[0]);
-    var qup=0;
-    for(var i=0;i<qlinks.length;i++){
-        qlinks[i].index=i;
-        qlists[i].style.display="none";
-        qlists[qup].style.display="block";
-        qlinks[qup].style.color="#fb601f";
-        qlinks[i].onclick=function(){
-            qlists[qup].style.display="none";
-            qlinks[qup].style.color="";
-            qup=this.index;
-            qlinks[qup].style.color="#fb601f";
-            qlists[qup].style.display="block";
-        }
-    }
-    /*imglist*/
-    var hnz_imglist1=$$(".hnz_img_list")[0];
-    var hnz_imglist2=$$(".hnz_img_list")[1];
-    imgBanner(hnz_imglist1);
-    imgBanner(hnz_imglist2);
-    function imgBanner(obj){
-        var hnz_imgs=$$("li",obj);
-        var hnz_iw=hnz_imgs[0].offsetWidth;
-        obj.style.width=hnz_imgs.length*(hnz_iw+20)+"px";
-        var hnz_tt=setInterval(move,3000);
-        hover(obj,function(){
-            clearInterval(hnz_tt);
-        },function(){
-            hnz_tt=setInterval(move,3000);
-        })
-        function move(){
-            animate(obj,{"marginLeft":-(hnz_iw+20)},function(){
-                obj.appendChild(getFirst(obj));
-                obj.style.marginLeft=0;
-            })
-        }
-    }
+$(function(){
+	//banner
+	var links=$(".hnz_banner_list a");
+	var lbtn=$(".hnz_btn_l");
+	var rbtn=$(".hnz_btn_r");
+	var bNum=0;
+	links.eq(0).css("zIndex",1)
+	var btt=setInterval(bMove,3000)
+	function bMove(){
+		bNum++;
+		if(bNum==links.length){
+			bNum=0;
+		}
+		links.css("zIndex",0);
+		links.eq(bNum).css("zIndex",1);
+	}
+	lbtn.hover(function(){
+		clearInterval(btt);
+	},function(){
+		btt=setInterval(bMove,3000)
+	});
+	rbtn.hover(function(){
+		clearInterval(btt);
+	},function(){
+		btt=setInterval(bMove,3000)
+	});
+	//lbtn
+	lbtn.click(function(){
+		bMove();
+	});
+	//rbtn
+	rbtn.click(function(){
+		bNum--;
+		if(bNum<0){
+			bNum=links.length-1;
+		}
+		links.css("zIndex",0);
+		links.eq(bNum).css("zIndex",1);
+	})
 
+
+	//å¿«é€Ÿå¯¼èˆª
+	var qList=$(".hnz_qnav_l_list");
+	var qbox=$(".hnz_qnav_l_box");
+	var qone=$(".hnz_qnav_l_box li");
+	var oneh=qone.eq(0).height();
+	qList.css("height",oneh*qone.length);
+	var btnU=$(".hnz_btn_up");
+	var btnD=$(".hnz_btn_down");
+	var outH=qbox.height();
+	var max=Math.ceil(qone.length/5);
+	var qnum=0;
+	if(qone.length*oneh>outH){
+		btnU.click(function(){
+			qnum++;
+			if(qnum>=max){
+				qnum=max-1;
+				return;
+			}else{
+				qList.animate({"marginTop":-5*qnum*oneh});
+			}
+		});
+		btnD.click(function(){
+			qnum--;
+			if(qnum<0){
+				qnum=0;
+				return;
+			}else{
+				qList.animate({"marginTop":-5*qnum*oneh});
+			}
+		})
+	}
+	// å¿«é€Ÿå¯¼èˆª  tab
+		var qlinks=$(".hnz_qnav_l_list a");
+		qlinks.eq(0).css('color','#fb601f');
+		var qlists=$(".hnz_qnav_r li");
+		qlinks.click(function(){
+			qlists.css("display","none");
+			qlists.eq(qlinks.index($(this))).css("display","block");
+			qlinks.css('color','');
+			$(this).css({color:'#fb601f'});
+		})
+	// img list
+	var ilist1=$(".hnz_img_list:eq(0)");
+	var ilist2=$(".hnz_img_list:eq(1)");
+	imgBan(ilist1);
+	imgBan(ilist2);
+	function imgBan(obj){
+		var imgs=obj.find('li');
+		var imgW=imgs.width();
+		obj.css({width:imgs.length*(imgW+20)});
+		var tt=setInterval(banMove,3000);
+		obj.hover(function(){
+            clearInterval(tt);
+        },function(){
+            tt=setInterval(banMove,3000);
+        })
+		function banMove(){
+			obj.animate({'marginLeft':-(imgW+20)},1000,function(){
+				obj.first().appendTo(obj);
+				obj.css('marginLeft',0);
+			})
+		}
+	}
 })
